18839705c9e7cdaee25c5050fc84fba230a7ced9
argituri <36637207+argituri@users.noreply.github.com>
2018-07-07 14:23:26 +0300

    Update main_javascript.js

--- javascript/main_javascript.js
+++ javascript/main_javascript.js
@@ -1,4 +1,4 @@
-var $ = require('jQuery');
+//var $ = require('jQuery'); this is not nodejs
 var elem = $("#nestedSmall");
 //	words for the changing word
 //	these could be separated into different groups and shown depending on the time of day or some other parameter.
