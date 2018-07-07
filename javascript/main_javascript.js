e0f6d719f98c7b27d76d271727738b568352dc9e
argituri <36637207+argituri@users.noreply.github.com>
2018-07-07 14:24:58 +0300

    Update main_javascript.js

--- javascript/main_javascript.js
+++ javascript/main_javascript.js
@@ -16,7 +16,7 @@ update(elem);
 
 function update(element){
 	//	Get a random value from array array (like word[random number between 0-0.999.. * arraylength])
-	var rand = word[Math.floor(Math.random() * myArray.length)];
+	var rand = word[Math.floor(Math.random() * word.length)];
 	var colors = ['#ff0000', '#00ff00', '#0000ff'];
 	var random_color = colors[Math.floor(Math.random() * colors.length)];
 	element.style.color = random_color;
